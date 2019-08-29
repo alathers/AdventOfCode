769139fbf1453cfbe0d13fdb240e295790a04591
Adam Lathers <alathers@gmail.com>
2019-08-29 02:06:29 -0700

    Finished through 4.2

--- 4/__init__.py
+++ 4/__init__.py
@@ -8,7 +8,7 @@ from guard import Guard,Guards
 def custom_sort(t):
     return t[1:10]
 
-with open('input.txt') as f:
+with open('all_input.txt') as f:
     infile = f.read().splitlines()
 f.close()
 
@@ -17,34 +17,73 @@ infile.sort(key=custom_sort)
 guards = Guards()
 
 for line in infile:
+    # print("{}".format(line))
     if 'Guard' in line:
         day, time, group, num, rem, rem2 = line.split(' ')
         guard = int(num[1:])
+        # print("Setting guard {}".format(guard))
     else:
         day, time, act, other = line.split(' ')
         hh, mm = time.split(':')
         mm = mm.replace(']', '')
         if act == 'falls':
-            stop = int(mm)
+            if hh == '00':
+                # only counting starting at midnight
+                rest = int(mm)
+            else:
+                # print("shifting to start at midnight")
+                rest = 0
+            wake = None
+            # print("tracking rest starting at {}".format(rest))
         elif act == 'wakes':
-            start = int(mm)
+            if hh == '00':
+                wake = int(mm)
+            else:
+                # print("shifting to end before midnight")
+                wake = 59
+            # print("tracking wake starting at {}".format(wake))
         try:
-            start
-            stop
+            rest
+            wake
             guard
-            if stop is not None and start is not None:
-                guards.tally_guard(guard, start, stop)
-                start = None
-                stop = None
+            if wake is not None and rest is not None:
+                # print("Adding minutes from {} to {} for {}".format(rest, wake, guard))
+                guards.tally_guard(guard, rest, wake)
+                rest = None
+                wake = None
         except NameError:
-            print("Exception hit")
+            # print("Exception hit")
+            pass
 
 
+# sleepiest = guards.sleepiest_guard
+# minute = guards.sleepiest_minute
+# total = sleepiest * minute
 
-sleepiest = guards.sleepiest_guard
-minute = guards.sleepiest_minute
-total = sleepiest * minute
-print("{} {} {}".format(sleepiest,minute,total))
+sleepy_guard = guards.sleepiest()
+sleepiest_minute = sleepy_guard.sleepiest_minute()
+print(sleepy_guard.id)
+print(sleepiest_minute)
+print(sleepy_guard.id * sleepiest_minute)
+print()
+
+# print("{} {} {}".format(sleepy_guard.id, sleepy_guard.sleepiest_minute,
+#                         sleepy_guard.id * sleepy_guard.sleepiest_minute ))
+# print("{} {} {}".format(sleepiest,minute,total))
 # print("{}".format(guards[guards.sleepiest_guard()].sleep))
-guards.dump_guards()
+# guards.dump_guards()
 
+# print( [ (k,v) for (k,v) in enumerate(guards.guards[sleepiest].minutes) ])
+guard_with_max_hits = guards.absolute_sleepiest_minute()
+absolute_sleepiest_minute = guard_with_max_hits.sleepiest_minute()
+times_on_minute = guard_with_max_hits.minutes[absolute_sleepiest_minute]
+which_minute = guard_with_max_hits.minutes.index(times_on_minute)
+print('---')
+print(guard_with_max_hits.id)
+print(absolute_sleepiest_minute)
+print(times_on_minute)
+print(which_minute)
+print(guard_with_max_hits.id * which_minute)
+print()
+
+guards.dump_guard_minutes()
--- 4/guard.py
+++ 4/guard.py
@@ -9,13 +9,15 @@ class Guard(object):
         self.id = id
         self.sleep = 0
         self.minutes = [0] * 60
+        self.minute_hashes = ["-"] * 60
 
-
-    def tally(self, start, end):
+    def tally(self, rest, wake):
         # print("Servicing tally on {} , {} {} {}".format(self.id, start, end, self.sleep))
-        sleep = start - end
+        sleep = wake - rest
         self.sleep += sleep
-        for i in range(end,start-1):
+        # print(rest, wake)
+        for i in range(rest, wake):
+            # print(i)
             self.minutes[i] += 1
 
     def sleepiest_minute(self):
@@ -28,28 +30,53 @@ class Guards(Guard):
 
     def __init__(self):
         self.max_sleep = -1
-        self.sleepiest_guard = -1
+        self.sleepiest_guard = None
         self.sleepiest_minute = -1
         self.guards = dict()
 
-    def tally_guard(self, id, start, end):
+    def tally_guard(self, id, rest, end):
         try:
             self.guards[id]
         except:
             self.guards[id] = Guard(id)
-        self.guards[id].tally(start,end)
-        if self.sleepiest_guard > 0 and self.guards[id].sleep > self.guards[self.sleepiest_guard].sleep:
-            self.sleepiest_guard = id
-        elif self.sleepiest_guard == -1:
-            self.sleepiest_guard = id
-        try:
-            self.sleepiest_minute = self.guards[self.sleepiest_guard].sleepiest_minute()
-        except:
-            pass
+        self.guards[id].tally(rest, end)
 
+    def sleepiest(self):
+        guards = self.guards
+        g_max = max ( guards, key=lambda x: guards[x].sleep)
+        return guards[g_max]
 
     def dump_guards(self):
         for g in self.guards:
             gd = self.guards[g]
             # print(gd.__dict__)
             print("{} {}  {}".format(g, gd.sleep, gd.sleepiest_minute()))
+
+    def absolute_sleepiest_minute(self):
+        guards = self.guards
+        g_max = max ( guards, key=lambda x: guards[x].minutes[guards[x].sleepiest_minute()])
+        return guards[g_max]
+
+    def dump_guard_minutes(self):
+        for g in self.guards:
+            gd = self.guards[g]
+            # print(gd.__dict__)
+            print("{} {}  {}".format(g, gd.sleep, gd.sleepiest_minute()))
+            print("{}".format(gd.minutes))
+
+
+
+"""
+Guests Part 1
+68183
+61531
+35184   <-
+
+
+Guest Part 2
+35472
+11824
+35472
+
+37886  <-
+"""
