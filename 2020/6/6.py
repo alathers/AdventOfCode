da38adbc556eb3a73ea4c6af57ab26dc8a24abe2
Adam Lathers <alathers@gmail.com>
2021-01-27 18:29:02 -0800

    Trying to get the last bits worked out for part2.  made some mistakes and managed a polymorphic answer for a while

--- 2020/6/6.py
+++ 2020/6/6.py
@@ -3,3 +3,18 @@
 
 with open('input.txt') as f:
     infile = f.read().splitlines()
+
+groups = set()
+ys = 0
+for g in infile:
+    if len(g):
+        [groups.add(y) for y in g ]
+    else:
+        # print(groups, len(groups), ys)
+        ys += len(groups)
+        groups = set()
+
+# make sure to count the last line    
+ys += len(groups)
+print(ys)
+
--- 2020/6/6_part2.py
+++ 2020/6/6_part2.py
@@ -3,3 +3,25 @@
 
 with open('input.txt') as f:
     infile = f.read().splitlines()
+
+group = []
+yeses = 0
+for n,g in enumerate(infile):
+    if len(g):
+        group.append(g)
+    if len(g) == 0 or n == len(infile)-1:
+        result = ''
+        for s in group:
+            if not result:
+                result = set([c for c in s])
+            else:
+                result = set([c for c in s]).intersection(result)
+        # print(result,len(result), ys)
+        yeses += len(result)
+        last = result
+        print(yeses, last)
+        result = ''
+        group = []
+
+yeses += len(last)
+print(yeses)
