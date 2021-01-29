9713fcd6b2c39f053a09617befb38223696ba408
Lathers, Adam (CORP) <Adam.Lathers@adp.com>
2021-01-29 12:24:39 -0800

    Starting prob 7.1

--- 2020/7/7.py
+++ 2020/7/7.py
@@ -1,5 +1,35 @@
 #!/usr/bin/env python3
+import re
 
+
+def getOuterBag(s):
+    outstring, contents = re.split('contain', s)
+    return outstring
+
+def getInnerBags(s):
+    outstring, contents = re.split('contain', s)
+    contents.strip('\.')
+    if contents == 'no other bags':
+        return {}
+    if re.match(','):
+        bags = contents.split(,)
+    else:
+        bags = ['contents']
+    for bag in bags:
+        number,description
+        
 
 with open('input.txt') as f:
     infile = f.read().splitlines()
+
+
+
+
+my_bag = 'shiny gold'
+outbags = {}
+
+
+
+for line in infile:
+    outstring = getOuterBag(line)
+    # print(outstring)    
