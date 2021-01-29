2964db07ee6cb29984497966a4f5a6eb30cc54dd
Lathers, Adam (CORP) <Adam.Lathers@adp.com>
2021-01-29 13:53:48 -0800

    Incremental changes to prep the class

--- 2020/7/7.py
+++ 2020/7/7.py
@@ -6,14 +6,14 @@ with open('input.txt') as f:
     infile = f.read().splitlines()
 
 outbags = {}
-can_contain = 0
+can_contain = []
 for line in infile:
     line = line.rstrip('.')
     outstring = my_bag.getOuterBag(line)
     inner_bags = my_bag.getInnerBags(line)
     outbags[outstring] = inner_bags
     if my_bag.description in inner_bags.keys():
-        can_contain += 1
+        can_contain.append()
     # print(inner_bags)
 
 print(len(can_contain))
